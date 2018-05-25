ddd3dfd066c7e385c261eb1691c143d970059af7
Mateo Puña Mercado <mateopuna98@gmail.com>
2018-05-25 18:54:51 -0400

    Add files via upload

---
+++ Movimiento/Xpositivo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Xpositivo : MonoBehaviour, IVirtualButtonEventHandler {
+
+    bool presionado = false;
+
+    public GameObject botonEjeXPositivo;
+    public float velocidad = 1;
+
+	// Use this for initialization
+
+	void Start () {
+
+
+        botonEjeXPositivo = GameObject.Find("xPositivo");//Obtiene el botón
+        botonEjeXPositivo.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);//Registra el cambio que ocurrirá cuando el botón sea presionado o soltado
+
+	}
+	
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+
+        Debug.Log("Botón presionado");
+        presionado = true;
+
+
+
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+
+        Debug.Log("Botón soltado");
+        presionado = false;
+
+
+    }
+	// Update is called once per frame
+	void Update () {
+
+        //Mueve el botón
+
+        if(presionado){
+
+            transform.Translate(Vector3.right * Time.deltaTime * velocidad);
+
+        }
+
+
+	}
+}
---
+++ Movimiento/Ynegativo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Ynegativo : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    bool presionado = false;
+
+    public GameObject botonEjeYNegativo;
+    public float velocidad = 1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        botonEjeYNegativo = GameObject.Find("xNegativo");//Obtiene el botón
+        botonEjeYNegativo.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);//Registra el cambio que ocurrirá cuando el botón sea presionado o soltado
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+
+        Debug.Log("Botón presionado");
+        presionado = true;
+
+
+
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+        Debug.Log("Botón soltado");
+        presionado = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (presionado)
+        {
+
+            transform.Translate(Vector3.back * Time.deltaTime * velocidad);
+
+        }
+    }
+}
