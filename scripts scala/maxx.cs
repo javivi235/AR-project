70d0f70372646c77aee5fbfa117078beb0405322
wowblast <38285313+wowblast@users.noreply.github.com>
2018-05-26 10:21:12 -0400

    Add files via upload
    scripts de cambio de escala de objeto

---
+++ scripts scala/maxx.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class maxx : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbButtonObject;
+    public GameObject casa;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        vbButtonObject = GameObject.Find("btmaxx");
+        casa = GameObject.Find("casa");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        casa.transform.localScale += new Vector3(0.1f, 0f, 0f);
+        
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+
+
+}
+
---
+++ scripts scala/miny.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class miny : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbButtonObject;
+    public GameObject casa;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        vbButtonObject = GameObject.Find("btminy");
+        casa = GameObject.Find("casa");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        casa.transform.localScale += new Vector3(0f, 0f, -0.1f);
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+
+
+}
+
