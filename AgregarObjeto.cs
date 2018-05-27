8bce48f045bc401ee17e15b9b7c489d21d8638f1
AngelUrq <angel4441916@gmail.com>
2018-05-26 20:29:51 -0400

    Script agregar objeto

---
+++ AgregarObjeto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class AgregarObjeto : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject boton;
+    public GameObject modelo;
+
+    public Transform target;
+    
+    private bool botonPresionado;
+    private bool crear;
+
+    private GameObject objeto;
+
+    void Start() {
+        boton = GameObject.Find("BotonAgregar");
+        boton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        botonPresionado = false;
+        crear = true;
+    }
+    
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+        if(!botonPresionado && crear) {
+            objeto = Instantiate(modelo, target.position, target.rotation);
+            botonPresionado = true;
+        } else {
+            Destroy(objeto);
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        crear = !crear;
+        botonPresionado = false;
+    }
+
+}
