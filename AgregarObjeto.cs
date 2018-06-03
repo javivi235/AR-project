ff32c0aaa2843ffa8996803740c63ee50f1a65ef
AngelUrq <angel4441916@gmail.com>
2018-06-03 16:42:06 -0400

    Ground plane agregado

--- AgregarObjeto.cs
+++ AgregarObjeto.cs
@@ -4,31 +4,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class AgregarObjeto : MonoBehaviour, IVirtualButtonEventHandler {
+public class ScriptAgregarObjeto : MonoBehaviour, IVirtualButtonEventHandler {
 
     public GameObject boton;
     public GameObject modelo;
 
-    public Transform target;
-    
+    private GameObject target;
+
     private bool botonPresionado;
     private bool crear;
 
     private GameObject objeto;
 
     void Start() {
-        boton = GameObject.Find("BotonAgregar");
+        boton = GameObject.Find("BtnAgregarObjeto");
         boton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        target = GameObject.Find("Ground Plane Stage");
         botonPresionado = false;
         crear = true;
     }
-    
+
     public void OnButtonPressed(VirtualButtonBehaviour vb) {
+        Vector3 posicion = target.transform.position;
+        posicion.y += 0.5f; 
+
         if(!botonPresionado && crear) {
-            objeto = Instantiate(modelo, target.position, target.rotation);
+            objeto = Instantiate(modelo, posicion, target.transform.rotation);
+            objeto.transform.parent = target.transform;
             botonPresionado = true;
         } else {
-            Destroy(objeto);
+            // Destroy(objeto);
         }
     }
 
