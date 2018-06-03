4c62ac6a702fa7e76064e3d31d59cb1c5af4f120
javivi235 <38427576+javivi235@users.noreply.github.com>
2018-06-02 23:46:08 -0400

    Add files via upload
    scripts necesarios para guardado

---
+++ CrearProyecto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CrearProyecto : MonoBehaviour {
+
+	public void Crear(InputField a){
+		if(a.text != "") {
+			
+			GameObject Name = GameObject.Find("Name");
+			Nombre nombre = Name.GetComponent<Nombre>();
+			Nombre.proyecto = a.text;
+			Debug.Log( Nombre.proyecto);
+			SceneManager.LoadScene("ARNewscene");	
+		
+		}
+	}
+}
