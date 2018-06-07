f59a2edb7b6354afd664d4f06733446d645fe4a9
javivi235 <38427576+javivi235@users.noreply.github.com>
2018-06-06 23:03:21 -0400

    Add files via upload
    guardado en android arreglado

--- Guardado.cs
+++ Guardado.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 using Vuforia;
 using System.IO;
 
-public class Guardado : MonoBehaviour, IVirtualButtonEventHandler {
+public class Guardado : MonoBehaviour, IVirtualButtonEventHandler
+{
 
-	public GameObject vbButtonObject;
-   
+    public GameObject vbButtonObject;
 
-	// Use this for initialization
-	void Start () {
-		
-		vbButtonObject = GameObject.Find("Guardar");
-		
-		vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
- 	public void OnButtonPressed(VirtualButtonBehaviour vb)
+    // Use this for initialization
+    void Start()
+    {
+
+        vbButtonObject = GameObject.Find("Guardar");
+
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         GameObject name = GameObject.Find("Name");
         Nombre nombre = name.GetComponent<Nombre>();
@@ -30,28 +33,29 @@ public class Guardado : MonoBehaviour, IVirtualButtonEventHandler {
         Debug.Log("Guardando");
         GameObject[] c = GameObject.FindGameObjectsWithTag("Obj");
         Debug.Log(c.Length);
-        string path = "Assets/Proyectos/" + Nombre.proyecto + ".txt";
-        if (System.IO.File.Exists(path))
+            string fileName = "";
+    	fileName = Application.persistentDataPath + "/" + Nombre.proyecto + ".txt" ;
+        if (System.IO.File.Exists(fileName))
         {
-            System.IO.File.Delete(path);
+            System.IO.File.Delete(fileName);
         }
-        StreamWriter writer = new StreamWriter(path, true);
+	StreamWriter fileWriter = new StreamWriter(fileName, true);
 
 
         foreach (GameObject go in c)
         {
 
-            writer.WriteLine(go.transform.name + "|" + go.transform.position.x + "|" + go.transform.position.y + "|" +
+            fileWriter.WriteLine(go.transform.name + "|" + go.transform.position.x + "|" + go.transform.position.y + "|" +
             go.transform.position.z + "|" + go.transform.rotation.eulerAngles.x + "|" + go.transform.rotation.eulerAngles.y + "|" + go.transform.rotation.eulerAngles.z + "|" +
             go.transform.lossyScale.x + "|" + go.transform.lossyScale.y + "|" + go.transform.lossyScale.z + "\n");
 
 
         }
-        writer.Close();
+        fileWriter.Close();
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        
+
     }
 }
