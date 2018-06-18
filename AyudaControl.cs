f745c3ad1d469f993af6a0eeff865e64278b34f6
NancyOrellanaT <adrianaorellana34@gmail.com>
2018-06-18 11:09:21 -0400

    Funcionalidad de ayuda terminada

---
+++ AyudaControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AyudaControl : MonoBehaviour {
+
+    public GameObject ayuda;
+
+    private bool ayudaActivada;
+
+    // Use this for initialization
+    void Start () {
+        ayudaActivada = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if(Input.GetButtonDown("BtnTriangulo"))
+        if (Input.GetButtonDown("BtnR2"))
+        {
+            if (ayudaActivada)
+            {
+                ayuda.SetActive(false);
+                ayudaActivada = false;
+                menuPrincipal.SetActive(false);
+                menuAgregar.SetActive(false);
+            }
+            else
+            {
+                ayuda.SetActive(true);
+                ayudaActivada = true;
+                menuPrincipal.SetActive(false);
+                menuAgregar.SetActive(false);
+            }
+        }
+       
+    }
+}
